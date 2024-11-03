6a2c12dad1e500ae5413072fe840e3519f41045e
antonyakobi <antonyakobi@yandex.by>
2024-11-03 14:23:02 +0300

    first commit

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,26 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true
+        }
+    }
+    return false
+}
+
+function findCommonElements(array1, array2) {
+    const result = []
+    for (let i = 0; i < array1.length; i++) {
+        if (includesElement(array1, array2[i])) {
+            result.push(array2[i])
+        } 
+    }
+    return result
+}
+
+
+
+console.log(findCommonElements([1, 2, 3], [2, 3, 4]));
+console.log(findCommonElements([1, 2, 3, 4, 5], [2, 3, 4, 6, 7]))
