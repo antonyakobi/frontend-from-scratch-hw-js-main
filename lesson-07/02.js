736f4c275b8aad0b0ce62ad7bce7fcd0c7cb7a63
antonyakobi <antonyakobi@yandex.by>
2024-11-24 22:59:29 +0300

    lesson07 02

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,11 +4,14 @@
  */
 
 function isNumeric(str) {
-  // your code
+  if (str.trim() === '') {
+    return false
+  }
+  return !Number.isNaN(+str)
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
